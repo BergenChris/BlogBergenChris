04863a90af1b4d18456f37a9e98aae187ad3a8bb
BergenChris <159039547+BergenChris@users.noreply.github.com>
2024-04-09 12:26:12 +0000

    ok

---
+++ Milestone2/index.ts
@@ -0,0 +1,36 @@
+import readline from "readline-sync";
+import express  from "express";
+import {Songs} from "./interfaces/songs";
+import ejs from "ejs";
+
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("port", 3000);
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+let songlist:Songs[]=[];
+
+
+app.get("/",(req,res)=>{
+    res.render("index",
+    {
+        songs:songlist
+    });
+  });
+  
+
+
+
+app.listen(app.get("port"), async () => {
+    let response:Response = await fetch("https://raw.githubusercontent.com/BergenChris/Milestone/main/Milestone1/songs.json");
+    songlist= await response.json();
+    console.log( "[server] http://localhost:" + app.get("port"));
+  });
+
+
+export {}
